ef1d193b38518ecedad1d592ab0c7bceeb3abe86
Vahid <vahid.hosseinioun@gmail.com>
2014-03-30 12:38:57 -0400

    adding the bat file.

---
+++ Roboilp/Compile Config/cli_for_roboilp.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+
+start "cd C:\Users\Vahid\workspace\adt\Roboilp\bin && java -cp "C:\Users\Vahid\workspace\adt\Roboilp\bin\;C:\Users\Vahid\workspace\adt\Roboilp\lib\jpl.jar" roboILP.RoboILP -theory "D:\\Dropbox\\2014 - Winter\\Software Agents\\RoboCup Project\\Final Project\\main\\aleph\\sims\\ego\\krislet\\rules\\krislet.const-ego.1000" -v 2 > log.txt"
